0e8155764273b95c3deef753c7d4630e9f5cafa0
peter-mwaura <peterkaranjagitz@gmail.com>
2025-10-05 17:24:31 +0300

    chore(config): add Prisma module to register existing Prisma service
    Introduced PrismaModule to make PrismaService globally available.
    Integrated it with the NestJS dependency injection system for better modularity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
